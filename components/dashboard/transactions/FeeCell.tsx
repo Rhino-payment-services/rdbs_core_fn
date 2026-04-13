4ee5277cf891cb3aedda0cf28cb10494fc2ff28b
Jim Ntare <jim.ntare@gmail.com>
2026-04-13 12:56:49 +0300

    deploy

--- components/dashboard/transactions/FeeCell.tsx
+++ components/dashboard/transactions/FeeCell.tsx
@@ -2,6 +2,7 @@
 
 import { TableCell } from '@/components/ui/table'
 import { formatAmount } from '@/lib/utils/transactions'
+import { getDisplayNetAmount } from '@/lib/utils/transactionNetDisplay'
 
 interface FeeCellProps {
   transaction: any
@@ -51,62 +52,12 @@ export const NetAmountCell = ({ transaction }: FeeCellProps) => {
     )
   }
 
-  // For non-sweep CREDIT legs (receiver side)
-  if (transaction.direction !== 'DEBIT') {
-    return (
-      <TableCell className="font-medium text-green-600">
-        {formatAmount(Number(transaction.netAmount))}
-      </TableCell>
-    )
-  }
-
-  // For DEBIT legs (sender side), Net Amount = amount + all fees (total debited)
-  const amount = Number(transaction.amount) || 0
-  const feeBreakdown = metadata.feeBreakdown || {}
-
-  const rukapayFeeFromBreakdown = feeBreakdown.rukapayFee || 0
-  const rukapayFee = rukapayFeeFromBreakdown > 0
-    ? rukapayFeeFromBreakdown
-    : (Number(transaction.rukapayFee) || 0)
-
-  const partnerFeeFromBreakdown = feeBreakdown.partnerFee || feeBreakdown.thirdPartyFee || 0
-  const thirdPartyFee = partnerFeeFromBreakdown > 0
-    ? partnerFeeFromBreakdown
-    : (Number(transaction.thirdPartyFee) || 0)
-
-  const govTaxFromBreakdown = feeBreakdown.governmentTax || feeBreakdown.govTax || 0
-  const governmentTax = govTaxFromBreakdown > 0
-    ? govTaxFromBreakdown
-    : (Number(transaction.governmentTax) || 0)
-
-  const processingFee = feeBreakdown.processingFee || Number(transaction.processingFee) || 0
-  const networkFee = feeBreakdown.networkFee || Number(transaction.networkFee) || 0
-  const complianceFee = feeBreakdown.complianceFee || Number(transaction.complianceFee) || 0
-  const telecomBankCharge = feeBreakdown.telecomBankCharge || 0
-
-  let calculatedTotalFees = rukapayFee + thirdPartyFee + governmentTax + processingFee + networkFee + complianceFee + telecomBankCharge
-
-  if (feeBreakdown.totalFee !== undefined && feeBreakdown.totalFee !== null) {
-    calculatedTotalFees = Number(feeBreakdown.totalFee)
-  }
-
-  let finalTotalFee = calculatedTotalFees
-
-  if (finalTotalFee === 0) {
-    const fee = Number(transaction.fee) || 0
-    if (fee > 0) {
-      finalTotalFee = fee
-    } else {
-      const netAmount = Number(transaction.netAmount) || 0
-      if (netAmount > 0 && amount !== netAmount) {
-        finalTotalFee = amount - netAmount
-      }
-    }
-  }
+  // Use the shared helper so table, modal, and exports always match.
+  const netForDisplay = getDisplayNetAmount(transaction)
 
   return (
     <TableCell className="font-medium text-green-600">
-      {formatAmount(amount + finalTotalFee)}
+      {netForDisplay == null ? '-' : formatAmount(netForDisplay)}
     </TableCell>
   )
 }
